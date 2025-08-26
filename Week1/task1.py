893e4180d0e69373b7b53fca16794e0b3a22cbdc
Rohma Masood <farkhandaakhter22@gmail.com>
2025-08-27 01:53:56 +0500

    Create task1.py

---
+++ Week1/task1.py
@@ -0,0 +1,8 @@
+#Task 1:
+print("Hello, World!")
+print("My name is Rohma Masood.")
+print("I am currently studying Mechatronics and Control Engineering at UET Lahore.")
+print("I started my journey in the MCT department on December 5, 2022.")
+print("I have a strong base in robotics, automation, embedded systems and AI.")
+print("I enjoy working on hands-on projects that involve electronics, coding and system design.")
+print("I'm always eager to learn new skills and explore how technology can solve real-world problems.")
